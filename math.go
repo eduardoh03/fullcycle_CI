453fd378ea9f13aa2a0db9c38a3a38a842155e27
Eduardo <eduhenriquesrs@gmail.com>
2024-01-31 00:26:33 -0300

    feat: Added three other go calculator operations.

--- math.go
+++ math.go
@@ -4,8 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println(soma(112, 10))
+	fmt.Println(subtract(112, 10))
+	fmt.Println(multiply(112, 10))
+	fmt.Println(divide(112, 10))
 }
 
 func soma(a int, b int) int {
 	return a + b
+}
+
+func subtract(a int, b int) int {
+	return a - b
+}
+func multiply(a int, b int) int {
+	return a * b
+}
+func divide(a int, b int) int {
+	return a / b
 }
--- math_test.go
+++ math_test.go
@@ -10,3 +10,28 @@ func TestSoma(t *testing.T) {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
+
+func TestSubtract(t *testing.T) {
+
+	total := subtract(15, 15)
+
+	if total != 0 {
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 0)
+	}
+}
+func TestMultiply(t *testing.T) {
+
+	total := multiply(15, 15)
+
+	if total != 225 {
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 225)
+	}
+}
+func TestDivide(t *testing.T) {
+
+	total := divide(15, 15)
+
+	if total != 1 {
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 1)
+	}
+}
